abb44741d5927037ede184106d54a88bc1a8fcde
olivier.pitton <olivier.pitton@gmail.com>
2016-03-24 20:55:43 +0100

    Real singleton

--- Positions/src/main/java/com/l3dant/dao/ConnexionMongo.java
+++ Positions/src/main/java/com/l3dant/dao/ConnexionMongo.java
@@ -4,17 +4,20 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 
 public class ConnexionMongo {
-	private static String url = "127.0.0.1";
-	private static MongoClient mongoClient;
-	private static MongoDatabase mongoDatabase;
+	private static final String url = "127.0.0.1";
 	
 	//Singleton
 	public static MongoDatabase getDatabase(){
-		if(mongoDatabase == null){
-			mongoClient = new MongoClient(url);
-			mongoDatabase = mongoClient.getDatabase("Positions");
-		}
-		return mongoDatabase;
+		return Init.mongoDatabase;
+	}
+
+	// Real singleton implementation. As this class is static, it won't be loaded when "ConnexionMongo" will be.
+	// So this DB connexion will be made when someone will call getDatabase.
+	// Some doc here about the Holder : http://thecodersbreakfast.net/index.php?post/2008/02/25/26-de-la-bonne-implementation-du-singleton-en-java
+	private static class Init {
+
+		private static final MongoDatabase mongoDatabase = new MongoClient(url).getDatabase("Positions");
+
 	}
 	
 }
